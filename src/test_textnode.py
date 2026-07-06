3971275f04927730dc3feedcbb401c918eb3283d
Twigg96 <96mhalwig@gmail.com>
2026-07-06 11:30:21 -0400

    Added test.sh with test_texttype

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_noteq(self):
+        node3 = TextNode("Yeps", TextType.CODE_TEXT)
+        node4 = TextNode("pey", TextType.ITALIC)
+        self.assertNotEqual(node3, node4)
+
+    def test_none(self, url=None):
+        node5 = TextNode("Yeps", TextType.CODE_TEXT)
+        self.assertIsNone(node5.url)
+
+    def test_NotNone(self):
+        node6 = TextNode("Yeps", TextType.CODE_TEXT, url="yep.com")
+        self.assertIsNotNone(node6.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
