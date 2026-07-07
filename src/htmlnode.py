8887b63bf4269baa331062d4fce68e33ee3ae4a8
Twigg96 <96mhalwig@gmail.com>
2026-07-07 15:58:58 -0400

    Added ParentNode with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,3 +36,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag")
+        elif self.children is None:
+            raise ValueError("No children")
+        else:
+            opening = f"<{self.tag}{self.props_to_html()}>"
+            closing = f"</{self.tag}>"
+            middle = ""
+            for c in self.children:
+                middle += c.to_html()
+            return opening + middle + closing
