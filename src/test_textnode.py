496c85ac4405a5b925a712cd80695cf830763d74
Twigg96 <96mhalwig@gmail.com>
2026-07-08 16:25:50 -0400

    Added tex_node_to_html_node and test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -20,6 +20,12 @@ class TestTextNode(unittest.TestCase):
     def test_NotNone(self):
         node6 = TextNode("Yeps", TextType.CODE_TEXT, url="yep.com")
         self.assertIsNotNone(node6.url)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from logging import raiseExceptions
+from htmlnode import LeafNode, HTMLNode
 
 
 class TextType(Enum):
@@ -25,3 +27,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text, None)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text, None)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text, None)
+    elif text_node.text_type == TextType.CODE_TEXT:
+        return LeafNode("code", text_node.text, None)
+    elif text_node.text_type == TextType.LINKS:
+        full_url = {"href": text_node.url}
+        return LeafNode("a", text_node.text, full_url)
+    elif text_node.text_type == TextType.IMAGES:
+        img_text = {"src": text_node.url, "alt": text_node.text}
+        return LeafNode("img", "", img_text)
+    else:
+        raise ValueError(f"Invalid text type {text_node.text_type}")
