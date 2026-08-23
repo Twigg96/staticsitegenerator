3e7bc4067ded10e34177fca990361fd08224d109
Twigg96 <96mhalwig@gmail.com>
2026-08-23 11:06:12 -0400

    Added copying functions to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 from textnode import *
+import os, shutil
+
+
+def check_directory(destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+
+
+def copy_content(source, destination):
+    items = os.listdir(source)
+    for item in items:
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+            print(f"Copying {source_path} to {destination_path}")
+        else:
+            copy_content(source_path, destination_path)
 
 
 def main():
-    node = TextNode("yep", TextType.BOLD, "yeps.com")
-    print(node)
+    check_directory("destination")
+    copy_content("static", "public")
 
 
 main()
